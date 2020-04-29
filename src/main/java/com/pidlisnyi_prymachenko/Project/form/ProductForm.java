43c262a9f03474b0c7d32d84ceac735ca279682c
Illya <illyaprimak@gmail.com>
2020-04-29 17:59:26 +0300

    add form and valdator

---
+++ src/main/java/com/pidlisnyi_prymachenko/Project/form/ProductForm.java
@@ -0,0 +1,66 @@
+package com.pidlisnyi_prymachenko.Project.form;
+
+import com.pidlisnyi_prymachenko.Project.entity.Product;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProductForm {
+    private String code;
+    private String name;
+    private double price;
+
+    private boolean newProduct = false;
+
+    // Upload file.
+    private MultipartFile fileData;
+
+    public ProductForm() {
+        this.newProduct= true;
+    }
+
+    public ProductForm(Product product) {
+        this.code = product.getCode();
+        this.name = product.getName();
+        this.price = product.getPrice();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public MultipartFile getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(MultipartFile fileData) {
+        this.fileData = fileData;
+    }
+
+    public boolean isNewProduct() {
+        return newProduct;
+    }
+
+    public void setNewProduct(boolean newProduct) {
+        this.newProduct = newProduct;
+    }
+
+}
---
+++ src/main/java/com/pidlisnyi_prymachenko/Project/validator/ProductFormValidator.java
@@ -0,0 +1,46 @@
+package com.pidlisnyi_prymachenko.Project.validator;
+
+import com.pidlisnyi_prymachenko.Project.dao.ProductDAO;
+import com.pidlisnyi_prymachenko.Project.entity.Product;
+import com.pidlisnyi_prymachenko.Project.form.ProductForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class ProductFormValidator implements Validator {
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    // This validator only checks for the ProductForm.
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz == ProductForm.class;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ProductForm productForm = (ProductForm) target;
+
+        // Check the fields of ProductForm.
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "code", "NotEmpty.productForm.code");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.productForm.name");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "price", "NotEmpty.productForm.price");
+
+        String code = productForm.getCode();
+        if (code != null && code.length() > 0) {
+            if (code.matches("\\s+")) {
+                errors.rejectValue("code", "Pattern.productForm.code");
+            } else if (productForm.isNewProduct()) {
+                Product product = productDAO.findProduct(code);
+                if (product != null) {
+                    errors.rejectValue("code", "Duplicate.productForm.code");
+                }
+            }
+        }
+    }
+
+}
